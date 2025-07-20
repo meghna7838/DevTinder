11534a414f2bb220bc72a93039c515aa3325d408
meghna7838 <meghnatandon7838@gmail.com>
2025-07-20 19:03:10 +0530

    Basic code functionality

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+app.listen(7777,()=>{
+    console.log("Hello waiting for req");
+});
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from server1");
+});
+
+app.use("/Meghna",(req,res)=>{
+    res.send("Hello this is meghna we love u");
+});
+
+app.use("/",(req,res)=>{
+    res.send("Hello from server");
+});
+
+
+

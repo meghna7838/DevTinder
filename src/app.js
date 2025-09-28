533a4416ae22126d110d9533805e5a8b3ca1b846
meghna7838 <meghnatandon7838@gmail.com>
2025-09-28 13:11:24 +0530

    DONE

--- src/app.js
+++ src/app.js
@@ -1,21 +1,49 @@
 const express = require("express");
 
 const app = express();
-app.listen(7777,()=>{
-    console.log("Hello waiting for req");
-});
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from server1");
-});
-
-app.use("/ab?cd",(req,res)=>{
-    res.send("Hello this is meghna we love u");
-});
-
-// app.use("/",(req,res)=>{
-//     res.send("Hello from server");
+// app.use("/test",(req,res)=>{
+//     res.send("HELLO FROM TEST");
 // });
 
+// app.use("/trying",(req,res)=>{
+//     res.send("HELLO FROM TRYING");
+// })
+// app.use("/",(req,res)=>{
+//     res.send("HELLO fROM /");
+// });
 
+app.get(
+    "/user",(req,res)=>{
+        res.send("USER GET");
+    }
+);
+
+app.post(
+    "/user",(req,res)=>{
+        res.send("USER PosT")
+    }
+);
+
+app.put(
+    "/user",(req,res)=>{
+        res.send("USER PUT")
+    }
+);
+
+app.patch(
+    "/user",(req,res)=>{
+        res.send("USER PATCH")
+    }
+);
+
+app.delete(
+    "/user",(req,res)=>{
+        res.send("USER DELETE");
+    }
+);
+
+app.listen(7777,()=>{
+    console.log("SERVER IS RUNNING");
+});
 
